bb1fcb37f962167f7a331bb3b4017f39c7986206
ctosullivan <75556293+ctosullivan@users.noreply.github.com>
2024-10-30 07:41:31 +0100

    Second attempt at packaging - V0.1

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from distutils.core import setup, find_packages
 
 setup(
     package_dir={'':'src'},
@@ -7,6 +7,16 @@ setup(
     entry_points={
         'console_scripts': [
             'wghelper = wghelper.command_line:main',
-        ]
-    }
+        ]},
+    version=0.1,
+    license=MIT,
+    description = "CLI tool to help solve various word games using logical and regular expressions",
+    author = "Cormac O' Sullivan",
+    author_email= 'cormac@cosullivan.dev',
+    url = "https://github.com/ctosullivan/Word-Game-Helper",
+    classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ],
 )
